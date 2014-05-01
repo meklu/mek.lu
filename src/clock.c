bcf36ba866c66e26173bad52796d4b56719f593f
Jørgen P. Tjernø <jorgenpt@gmail.com>
2014-04-30 17:50:30 -0700

    OSX: Don't cache clock service.
    Caching the clock service makes it wonky when you fork(), as they are
    per-process.

--- src/clock.c
+++ src/clock.c
@@ -6,17 +6,11 @@
 #include <mach/mach.h>
 
 static uint64_t timebase_factor = 0;
-static clock_serv_t cclock = MACH_PORT_NULL;
-
 int clock_gettime(int clock_id, struct timespec *ts) {
 	if (timebase_factor == 0) {
 		mach_timebase_info_data_t timebase_info;
 		(void)mach_timebase_info(&timebase_info);
 		timebase_factor = timebase_info.numer / timebase_info.denom;
-	}
-
-	if (cclock == MACH_PORT_NULL) {
-		host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
 	}
 
 	switch (clock_id)
@@ -30,8 +24,13 @@ int clock_gettime(int clock_id, struct timespec *ts) {
 		}
 		case CLOCK_REALTIME:
 		{
+			clock_serv_t cclock = MACH_PORT_NULL;
 			mach_timespec_t mts;
+
+			host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
 			clock_get_time(cclock, &mts);
+			mach_port_deallocate(mach_task_self(), cclock);
+
 			ts->tv_sec = mts.tv_sec;
 			ts->tv_nsec = mts.tv_nsec;
 			return 0;
